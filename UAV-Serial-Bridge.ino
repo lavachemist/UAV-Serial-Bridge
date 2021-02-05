36540f69b81abe087952075f5213e8ae0e91662a
Lava Chemist <thelavachemist@gmail.com>
2021-02-04 23:25:28 -0500

    Added WifiManager

---
+++ UAV-Serial-Bridge.ino
@@ -0,0 +1,182 @@
+// LED will blink when in config mode
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include "config.h"
+#include <esp_wifi.h>
+#include <WiFi.h>
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13 // ESP32 DOES NOT DEFINE LED_BUILTIN
+#endif
+
+int LED = LED_BUILTIN;
+
+void tick()
+{
+  //toggle state
+  digitalWrite(LED, !digitalRead(LED));     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+HardwareSerial Serial_one(1);
+HardwareSerial Serial_two(2);
+HardwareSerial* COM[NUM_COM] = {&Serial, &Serial_one , &Serial_two};
+
+#define MAX_NMEA_CLIENTS 4
+#ifdef PROTOCOL_TCP
+#include <WiFiClient.h>
+WiFiServer server_0(SERIAL0_TCP_PORT);
+WiFiServer server_1(SERIAL1_TCP_PORT);
+WiFiServer server_2(SERIAL2_TCP_PORT);
+WiFiServer *server[NUM_COM]={&server_0,&server_1,&server_2};
+WiFiClient TCPClient[NUM_COM][MAX_NMEA_CLIENTS];
+#endif
+
+
+uint8_t buf1[NUM_COM][bufferSize];
+uint16_t i1[NUM_COM]={0,0,0};
+
+uint8_t buf2[NUM_COM][bufferSize];
+uint16_t i2[NUM_COM]={0,0,0};
+
+uint8_t BTbuf[bufferSize];
+uint16_t iBT =0;
+
+void setup() {
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  //set led pin as output
+  pinMode(LED, OUTPUT);
+  // start ticker with 0.5 because we start in AP mode and try to connect
+  ticker.attach(0.6, tick);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
+  //reset settings - for testing
+  // wm.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wm.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wm.autoConnect()) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.restart();
+    delay(1000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  ticker.detach();
+  //keep LED on
+  digitalWrite(LED, LOW);
+
+    delay(500);
+  
+  COM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
+  COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
+  COM[2]->begin(UART_BAUD2, SERIAL_PARAM2, SERIAL2_RXPIN, SERIAL2_TXPIN);
+  
+  if(debug) COM[DEBUG_COM]->println("\nWiFi connected");   
+
+  #ifdef PROTOCOL_TCP
+  COM[0]->println("Starting TCP Server 1");  
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 1");  
+  server[0]->begin(); // start TCP server 
+  server[0]->setNoDelay(true);
+  COM[1]->println("Starting TCP Server 2");
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 2");  
+  server[1]->begin(); // start TCP server 
+  server[1]->setNoDelay(true);
+  COM[2]->println("Starting TCP Server 3");
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 3");  
+  server[2]->begin(); // start TCP server   
+  server[2]->setNoDelay(true);
+  #endif
+
+  esp_err_t esp_wifi_set_max_tx_power(84);  // 84 * 0.25dbm
+}
+
+void loop() 
+{  
+
+#ifdef PROTOCOL_TCP
+  for(int num= 0; num < NUM_COM ; num++)
+  {
+    if (server[num]->hasClient())
+    {
+      for(byte i = 0; i < MAX_NMEA_CLIENTS; i++){
+        //find free/disconnected spot
+        if (!TCPClient[num][i] || !TCPClient[num][i].connected()){
+          if(TCPClient[num][i]) TCPClient[num][i].stop();
+          TCPClient[num][i] = server[num]->available();
+          if(debug) COM[DEBUG_COM]->print("New client for COM"); 
+          if(debug) COM[DEBUG_COM]->print(num); 
+          if(debug) COM[DEBUG_COM]->println(i);
+          continue;
+        }
+      }
+      //no free/disconnected spot so reject
+      WiFiClient TmpserverClient = server[num]->available();
+      TmpserverClient.stop();
+    }
+  }
+#endif
+ 
+  for(int num= 0; num < NUM_COM ; num++)
+  {
+    if(COM[num] != NULL)          
+    {
+      for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+      {               
+        if(TCPClient[num][cln]) 
+        {
+          while(TCPClient[num][cln].available())
+          {
+            buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from client (LK8000 app)
+            if(i1[num]<bufferSize-1) i1[num]++;
+          } 
+
+          COM[num]->write(buf1[num], i1[num]); // now send to UART(num):
+          i1[num] = 0;
+        }
+      }
+  
+      if(COM[num]->available())
+      {
+        while(COM[num]->available())
+        {     
+          buf2[num][i2[num]] = COM[num]->read(); // read char from UART(num)
+          if(i2[num]<bufferSize-1) i2[num]++;
+        }
+        // now send to WiFi:
+        for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+        {   
+          if(TCPClient[num][cln])                     
+            TCPClient[num][cln].write(buf2[num], i2[num]);
+        }
+        i2[num] = 0;
+      }
+    }    
+  }
+}
